95fcbcba22909d460e468a9e6f55c261ae710048
Ahmed Hemeda <7hemeda@gmail.com>
2024-12-17 23:48:50 +0200

    Add project files.

---
+++ Assignment 02/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Assignment_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //#region Q1
+            //Console.Write("Enter a number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("You entered: " + number);
+            //#endregion
+
+
+            //#region Q2
+            //string str = "123abc";
+            //try
+            //{
+            //    int num = Convert.ToInt32(str);
+            //    Console.WriteLine(num);
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("Error: Invalid input, cannot convert string to integer.");
+            //}
+            //#endregion
+
+
+            //#region Q3
+            //float num1 = 5.5f;
+            //float num2 = 2.3f;
+            //float result = num1 + num2;
+            //Console.WriteLine($"The result of the addition is: {result}");
+            //#endregion
+
+
+            //#region Q6
+            //string[] arr1 = { "apple", "banana" };
+            //string[] arr2 = arr1;
+            //arr1[0] = "grape";
+            //Console.WriteLine($"arr1[0]: {arr1[0]}, arr2[0]: {arr2[0]}");
+            //#endregion
+
+
+            //#region Q8
+            //double principal = 1000, rate = 5, time = 3;
+            //double interest = (principal * rate * time) / 100;
+            //Console.WriteLine($"The simple interest is: {interest}");
+            //#endregion
+
+
+            //#region Q9
+            //double weight = 70; // in kilograms
+            //double height = 1.75; // in meters
+            //double bmi = weight / (height * height);
+            //Console.WriteLine($"The BMI is: {bmi}");
+            //#endregion
+
+
+            //#region Q11
+            //DateTime date = new DateTime(2001, 11, 20);
+            //Console.WriteLine($"Today’s date : {date.Day} , {date.Month} , {date.Year}");
+            //Console.WriteLine($"Today's date : {date.Day}/{date.Month}/{date.Year}");
+            //Console.WriteLine($"Today's date : {date.Day}–{date.Month}–{date.Year}");
+            //#endregion
+
+
+            //#region Q12
+            //DateTime date = new DateTime(2024, 6, 14);
+            //Console.WriteLine($"The event is on {date:MM/dd/yyyy}");
+            //c) The event is on 06/14/2024
+            //#endregion
+
+
+            //#region Q13
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //f) A value 1 will be assigned to d.
+            //#endregion
+
+
+            //#region Q14
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+            ////d) 6 1
+            //#endregion
+
+
+            //#region Q15
+            //int num = 1, z = 5;
+
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+            ////b) 6 5
+            //#endregion
+
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Hemeda
+    {
+        // Entry Point
+        static void Main()
+        {
+            Console.Write(value: "Hello, World!");
+            Console.Write(value: "Hello, Ahmed!");
+            Console.Write(value: "Hello, Hemeda!");
+            Console.Write(value: "Hello, World!");
+            Console.Write(value: "Hello, World!");
+        }
+    }
+}
